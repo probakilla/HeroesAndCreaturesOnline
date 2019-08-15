8d0156d04519fb60bcf6b5c5a98dc69de00be122
Julien Pilleux <julien.pilleux@etu.u-bordeaux.fr>
2019-08-15 14:15:10 +0200

    Updates

---
+++ precache-manifest.114e5c1372e4ff7914083ce26026fc0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59a4832e858c631ab031783b9bbceca6",
+    "url": "/heroesAndCreatures/index.html"
+  },
+  {
+    "revision": "8ebc1826749944710dde",
+    "url": "/heroesAndCreatures/static/css/main.cd848eba.chunk.css"
+  },
+  {
+    "revision": "92fc3c82ecd6d546e429",
+    "url": "/heroesAndCreatures/static/js/2.373df350.chunk.js"
+  },
+  {
+    "revision": "8ebc1826749944710dde",
+    "url": "/heroesAndCreatures/static/js/main.f486bb23.chunk.js"
+  },
+  {
+    "revision": "b5a9a8ca81242e83a69b",
+    "url": "/heroesAndCreatures/static/js/runtime~main.2172c302.js"
+  }
+]);
